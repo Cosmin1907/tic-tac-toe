6b67c36e92d5bd5e26758d3cb9f332ef59ef008e
Cosmin Timofte <timoftecosmin@yahoo.com>
2024-01-31 17:30:47 +0000

    Add logic to check for winner in all directions

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,12 +72,52 @@ function turnClick() {
 function checkWinner() {
     console.log("I am checking")
 
-    //horizontally 
+    //check horizontally 
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns - 3; c++) {
             if (grid[r][c] != '') {
                 if (grid[r][c] == grid[r][c+1] && grid[r][c+1] == grid[r][c+2] && grid[r][c+2] == grid[r][c+3]) {
-                    console.log("Winner Found horizontally")
+                    console.log("Winner Found horizontally", r, c)
+                    endGame(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    //check vertically 
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 3; r++) {
+            if (grid[r][c] != '') {
+                if (grid[r][c] == grid[r+1][c] && grid[r+1][c] == grid[r+2][c] && grid[r+2][c] == grid[r+3][c]) {
+                    console.log("Winner Found vertically", r, c)
+                    endGame(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    //check anti diagonally 
+    for (let r = 0; r < rows - 3; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (grid[r][c] != '') {
+                if (grid[r][c] == grid[r+1][c+1] && grid[r+1][c+1] == grid[r+2][c+2] && grid[r+2][c+2] == grid[r+3][c+3]) {
+                    console.log("Winner Found anti diagonally", r, c)
+                    endGame(r, c);
+                    return;
+                }
+            }
+        }
+
+    }
+
+    // check diagonally 
+    for (let r = 3; r < rows; r++) {
+        for ( let c = 0; c < columns - 3; c++) {
+            if (grid[r][c] != '') {
+                if (grid[r][c] == grid[r-1][c+1] && grid[r-1][c+1] == grid[r-2][c+2] && grid[r-2][c+2] == grid[r-3][c+3]) {
+                    console.log("Winner Found diagonally", r, c)
                     endGame(r, c);
                     return;
                 }
